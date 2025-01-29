b6f7262cc95943042fea96b3f259afe00d6de7f4
Tharidu Sathsara <tharidusathsara200@gmail.com>
2025-01-29 23:27:30 +0530

    create admin panel 👍

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ function App() {
 
   return (
     <div>
-      <ProductCard img="https://img.drz.lazcdn.com/static/lk/p/ec2000f21d165007436608a699c4e9b8.jpg_400x400q80.jpg_.webp" name="HIFI SOUND" price="RS.1,450" description="2024 new M25 Gaming Earbuds Headset Low Latency In-ear Earbuds Power Bank Gaming Airpods Headphones Wireless Gaming Tws Earbuds"/>
-     
-      <ProductCard img="https://img.drz.lazcdn.com/static/lk/p/63f71442c3e2e206189eb9e43a67473b.jpg_400x400q75.jpg_.webp" name="HIFI SOUND" price="RS.1,450" description="2024 new M25 Gaming Earbuds Headset Low Latency In-ear Earbuds Power Bank Gaming Airpods Headphones Wireless Gaming Tws Earbuds"/>
     </div>
   )
 }
