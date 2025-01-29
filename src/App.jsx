59b6002a10fdb04a7d28239de16d5cb555178012
Tharidu Sathsara <tharidusathsara200@gmail.com>
2025-01-29 23:30:32 +0530

    download packages and create admin panel✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { MdOutlineSpeaker } from 'react-icons/md';
 import './App.css'
 import ProductCard from './components/productCard'
-
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark, FaRegUser } from "react-icons/fa";
 function App() {
  
 
   return (
-    <div>
+    <div className='w-full h-screen flex'>
+      <div className='w-[300px] h-full bg-green-200'>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+          <BsGraphDown/>
+          Dashboard
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+          <FaRegBookmark/>
+          Bookings
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+          <MdOutlineSpeaker/>
+          Items
+        </button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+          <FaRegUser/>
+          Users
+        </button>
+      </div>
+      <div className='w-full bg-red-900'>
+
+      </div>
     </div>
   )
 }
