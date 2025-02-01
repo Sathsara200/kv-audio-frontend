21c0e624340c5fec20429b289776c976ff76088a
Tharidu Sathsara <tharidusathsara200@gmail.com>
2025-02-01 20:55:36 +0530

    impliment login and connct with backend

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import AdminPage from './pages/admin/adminPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/homePage';
+import LoginPage from './pages/login/login';
+import { Toaster } from 'react-hot-toast';
 function App() {
  
 
   return (
     <BrowserRouter>
+      <Toaster position="top-right"/>
       <Routes path="/*">
+        <Route path="/login" element={<LoginPage/>}/>
         <Route path="/admin/*" element={<AdminPage/>}/>
         <Route path="/*" element={<HomePage/>}/>
       </Routes>
