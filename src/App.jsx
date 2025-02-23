7664d522582e05919fc51b14d655e30f4179a024
Tharidu Sathsara <tharidusathsara200@gmail.com>
2025-02-23 18:30:34 +0530

    change colors and and create image upload function 😊

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/home/homePage';
 import LoginPage from './pages/login/login';
 import { Toaster } from 'react-hot-toast';
 import RegisterPage from './pages/register/register';
+import Testing from './components/testing';
 function App() {
  
 
@@ -12,6 +13,7 @@ function App() {
     <BrowserRouter>
       <Toaster position="top-right"/>
       <Routes path="/*">
+        <Route path="/testing" element={<Testing/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
         <Route path="/admin/*" element={<AdminPage/>}/>
---
+++ src/utils/mediaUpload.jsx
@@ -0,0 +1,32 @@
+import { createClient } from "@supabase/supabase-js"
+
+const anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4aHZhdHNla29tYnZuYnpsbXFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzMDU0MzQsImV4cCI6MjA1NTg4MTQzNH0.PeWQnP3Z5t75lN_owrb3cqYYfoZMfM1dAxkvAG8gZBs"
+const supabase_url = "https://cxhvatsekombvnbzlmqh.supabase.co"
+
+const supabase = createClient(supabase_url,anon_key)
+
+export default function mediaUpload(file){
+  
+    return new Promise((resolve, reject)=>{
+        
+        if(file == null){
+           reject( "No file selected")
+        }
+
+        const timestamp = new Date().getTime();
+        const fileName = timestamp+file.name
+
+        supabase.storage.from("images").upload(fileName,file,{
+            cacheControl: '3600',
+            upsert: false,
+        })
+        .then(()=>{
+
+            const publicUrl = supabase.storage.from("images").getPublicUrl(file.name).data.publicUrl;
+            resolve(publicUrl)
+        }).catch(()=>{
+            reject("Error  uploading file")
+        })
+    });
+}
+
