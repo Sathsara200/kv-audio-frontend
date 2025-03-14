c6e134d6ccfebbb0bbf2ddfaaedf5137941283c4
Tharidu Sathsara <tharidusathsara200@gmail.com>
2025-03-15 00:36:15 +0530

    impliment order details page✅

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -6,8 +6,36 @@ import AdminItemsPage from "./adminItemsPage";
 import UpdateItemPage from "./updateItemPage";
 import AddItemPage from "./addItemPage";
 import AdminUsersPage from "./adminUsersPage";
+import AdminOrdersPage from "./adminBookingPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 export default function AdminPage(){
+    const [userValidated,setUserValidated] = useState(false);
+    useEffect(()=>{
+      const token = localStorage.getItem("token");
+      if(!token){
+        window.location.href = "/login";
+      }
+      axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/`,{
+        headers:{
+          Authorization: `Bearer ${token}`
+        }
+      }).then((res)=>{
+        console.log(res.data);
+        const user = res.data;
+        if(user.role == "admin"){
+          setUserValidated(true);        
+        }else{
+          window.location.href = "/";
+        }
+        
+      }).catch((err)=>{
+        console.error(err);
+        setUserValidated(false);
+      })
+    },[])
     return(
         <div className='w-full h-screen flex'>
           <div className='w-[200px] h-full bg-green-200'>
@@ -15,9 +43,9 @@ export default function AdminPage(){
               <BsGraphDown/>
               Dashboard
             </button>
-            <Link to="/admin/bookings" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+            <Link to="/admin/orders" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
               <FaRegBookmark/>
-              Bookings
+              Orders
             </Link>
             <Link to="/admin/items" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
               <MdOutlineSpeaker/>
@@ -29,13 +57,13 @@ export default function AdminPage(){
             </Link>
         </div>
         <div className='w-[calc(100vw-200px)]'>
-          <Routes path="/*">
-            <Route path="/bookings" element={<h1>Booking</h1>}/>
+        {userValidated&&<Routes path="/*">
+            <Route path="/orders" element={<AdminOrdersPage/>}/>
             <Route path="/users" element={<AdminUsersPage/>}/>
             <Route path="/items" element={<AdminItemsPage/>}/>
             <Route path="/items/add" element={<AddItemPage/>}/>
             <Route path="/items/edit" element={<UpdateItemPage/>}/>
-          </Routes>
+          </Routes>}
         </div>
       </div>
   )
