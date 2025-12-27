60a960eede705db1542b29922a138ae9f9f6d1e4
Sathsara200 <tharidusathsara200@gmail.com>
2025-12-27 14:32:56 +0530

    bug fix

--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const anon_key = "sb_publishable_g3RR3TqObmRkrGwN-yAJzw_ORMc2Iyh";
+const anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnaGhoY3h5eGZjaHdyb2hudGd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3MTcyOTksImV4cCI6MjA4MjI5MzI5OX0.xCIXzaGl-fP6iR-v68Bl0U60q7IPfKv9kTt5h7EfJMo";
 const supabase_url = "https://jghhhcxyxfchwrohntgu.supabase.co";
 
 const supabase = createClient(supabase_url,anon_key)
@@ -16,7 +16,7 @@ export default function mediaUpload(file){
         const timestamp = new Date().getTime();
         const fileName = timestamp + file.name;
 
-        supabase.storage.from("images").upload(fileName,file,{
+        supabase.storage.from("KV-Audio").upload(fileName,file,{
             cacheControl: '3600',
             upsert: false,
         })
